a25a211e7986628f34836434b37c598fa383cd5f
MSI GAMING <MSI GAMING@DESKTOP-B3NMJRK>
2021-04-09 09:52:16 +0530

    Add .gitignore and .gitattributes.

---
+++ JWTAuthentication_TokenBarer/Models/AppSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTAuthentication_TokenBarer
+{
+    public class AppSettings
+    {
+
+        public string Key { get; set; }
+
+    }
+}
---
+++ JWTAuthentication_TokenBarer/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTAuthentication_TokenBarer.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Fisrtname { get; set; }
+        public string Lastname { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Token { get; set; }
+    }
+}
